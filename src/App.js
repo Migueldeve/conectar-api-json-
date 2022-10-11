78141beb30566ccf78afdf2d8c70363f3a946269
Migueldeve <107196966+Migueldeve@users.noreply.github.com>
2022-10-10 20:26:33 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+const url = 'https://jsonplaceholder.typicode.com/users'
+const [users, setUsers] = useState()
+const fetchApi = async () => {
+  const response = await fetch(url)
+  const datos = await response.json();
+  setUsers(datos)
+}
+useEffect(() => {
+  fetchApi()
+}, [])
+  return (
+    <div className="App">
+    <Navbar />
+    <div className="datos">
+    { !users ? 'Cargando...' :
+    users.map((user,index) => {
+      return <h4 key={index}>{user.id}.{user.name}</h4>
+      })
+    }
+    </div>
+  </div>
+  );
+}
+
+export default App;
